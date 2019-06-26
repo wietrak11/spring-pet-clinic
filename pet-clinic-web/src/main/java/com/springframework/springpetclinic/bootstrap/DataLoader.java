faded05e48358b289e3c3b51dcfd73274442cc6c
Mateusz Wietrak <wietrak11@gmail.com>
2019-06-26 13:55:46 +0200

    Refactored Map Impl to abstract ID generation. Closes #23

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mateusz");
         owner1.setLastName("Wietrak");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Klaudia");
         owner2.setLastName("Dobroń");
 
@@ -39,14 +37,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Jan");
         vet1.setLastName("Kowalski");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Stefan");
         vet2.setLastName("Nowacki");
 
