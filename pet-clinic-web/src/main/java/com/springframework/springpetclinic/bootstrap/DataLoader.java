392d11b9bdf3f2986f239fd0973f47b737c9ff90
Mateusz <wietrak11@gmail.com>
2019-08-28 18:58:52 +0200

    Added Pets and Contact info to owners on startup. Closes #33.

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -1,13 +1,17 @@
 package com.springframework.springpetclinic.bootstrap;
 
 import com.springframework.springpetclinic.model.Owner;
+import com.springframework.springpetclinic.model.Pet;
 import com.springframework.springpetclinic.model.PetType;
 import com.springframework.springpetclinic.model.Vet;
 import com.springframework.springpetclinic.services.OwnerService;
 import com.springframework.springpetclinic.services.PetTypeService;
 import com.springframework.springpetclinic.services.VetService;
+import org.apache.tomcat.jni.Local;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -37,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Mateusz");
         owner1.setLastName("Wietrak");
+        owner1.setAddress("Jana Pawła 4");
+        owner1.setCity("Warszawa");
+        owner1.setTelephone("123123123");
+
+        Pet owner1Pet = new Pet();
+        owner1Pet.setPetType(savedDogPetType);
+        owner1Pet.setOwner(owner1);
+        owner1Pet.setBirthDate(LocalDate.now());
+        owner1Pet.setName("Oozaru");
+        owner1.getPets().add(owner1Pet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Klaudia");
         owner2.setLastName("Dobroń");
+        owner2.setAddress("Jana Pawła 4");
+        owner2.setCity("Warszawa");
+        owner2.setTelephone("123123123");
+
+        Pet owner2Pet = new Pet();
+        owner2Pet.setPetType(savedCatPetType);
+        owner2Pet.setOwner(owner2);
+        owner2Pet.setBirthDate(LocalDate.now());
+        owner2Pet.setName("śmieć");
+        owner2.getPets().add(owner2Pet);
 
         ownerService.save(owner2);
 
